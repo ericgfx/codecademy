bad5a2a343613ed97e25db265c79164f3ed31f71
ericgfx <ericgfx01@gmail.com>
2018-10-08 15:43:27 -0700

    ScreenSaver Python

---
+++ projects/ScreenSavers/ScreenMaintenance.py
@@ -0,0 +1,17 @@
+slideList = [
+{
+"filename": "Slide1.png",
+"content": 'KeyDates',
+"endDate": 03/03/2019,
+"Filler Status": false,
+"startDate":01/01/2018
+},{
+"filename": "Slide2.png",
+"content": 'MoveTeam',
+"endDate": 03/03/2019,
+"Filler Status": false,
+"startDate":01/01/2018
+}
+]
+
+print slideList[0]["endDate"]
